a6357fa4ff7c796cf687691983e5bef5a559d803
IceOnFire <antony.mistretta@gmail.com>
2021-05-16 10:12:53 +0200

    Create a single-component app

---
+++ src/HelloWorld.js
@@ -0,0 +1,13 @@
+// import React from "react";
+
+export default function App() {
+  // return React.createElement("p", { className: "hello" }, "Hello world!");
+
+  return <p className="hello">Hello world!</p>;
+}
+
+// class App {
+//   render() {
+//     return <p className="hello">Hello world!</p>;
+//   }
+// }
